a9c52149bd4359e75ecf181b7a5c0add5f4c10b3
simennb <simen.bastnes@gmail.com>
2016-10-23 21:53:49 +0200

    stuff

--- src/integrator.cpp
+++ src/integrator.cpp
@@ -16,7 +16,6 @@ void Integrator::integrateOneStep(SolarSystem &system)
     /* Forward Euler */
     if (strcmp(num_method,"Euler")==0)
     {
-        system.calculateForcesAndEnergy();
         for (CelestialBody &body : system.bodies()) {
             body.position += body.velocity*m_dt;
             body.velocity += body.force / body.mass * m_dt;
@@ -26,7 +25,6 @@ void Integrator::integrateOneStep(SolarSystem &system)
     /* Velocity Verlet */
     if (strcmp(num_method,"Verlet")==0)
     {
-
         for(CelestialBody &body : system.bodies()) {
             vec3 accel1 = body.force/body.mass; // acceleration before updating x(i) to x(i+1)
             body.position += body.velocity*m_dt + 0.5 * accel1 * pow(m_dt,2);
